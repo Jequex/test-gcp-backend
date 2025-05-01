8078e117c3776971a6ff9620e65e324ffbf6a7e9
/Jequex <onyejekwe.harddisk20@gmail.com>
2025-05-02 00:17:12 +0100

    update

--- app.js
+++ app.js
@@ -6,4 +6,4 @@ app.get('/', (req, res) => {
   return res.status(200).send({ message: 'Hello, world!' });
 });
 
-app.listen(8080, (req, res) => console.log('listening on port 8080'));
+app.listen(8000, (req, res) => console.log('listening on port 8000'));
