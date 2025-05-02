a35597b6abec13df830b154ef6b67bc9718ef8fd
/Jequex <onyejekwe.harddisk20@gmail.com>
2025-05-02 12:53:16 +0100

    adding cors

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(cors({ origin: '*' }));
 
 app.get('/', (req, res) => {
   return res.status(200).send({ message: 'Hello, world!' });
